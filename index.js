997563138afcea45f85e3a35fb1d50af835f851b
thsp1900 <thsp1900@student.miun.se>
2026-02-13 12:57:52 +0100

    Set up MongoDB

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo').default || require('connect-mongo');
 const dotenv = require("dotenv");
 const { BASE_URL } = require("./config");
+const db = require('./db-handler');
 
 dotenv.config();
 
@@ -30,8 +31,16 @@ app.use(session({
 }))
 
 const routes = require("./routes/routes");
-
 app.use(BASE_URL, routes);
+
+// Connect to the MongoDB database before starting the server.
+db.connect()
+  .then(() => {
+    console.log('Connected to the MongoDB database.');
+  })
+  .catch(err => {
+    console.error('Error with the initial connection to the MongoDB database: ' + err.message);
+  });
 
 app.get("/", (req, res) => {
   res.send("Backend is working!");
