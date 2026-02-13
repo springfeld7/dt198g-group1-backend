7fc15fc2c9ebdc53205af03619f7021282be6af6
thsp1900 <thsp1900@student.miun.se>
2026-02-13 08:55:43 +0100

    Refine middleware function

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,15 @@
 /**
- * A middleware that checks if the userId value is set in the session object.
+ * A middleware that checks if userId is set in the session object.
  *
  * @param {Object} req - The request object.
  * @param {Object} res - The response object.
  * @param {Function} next - The next middleware function.
  */
 const authenticate = (req, res, next) => {
-    if (req.session.userId) {
-        next();
-    } else {
-        res.status(401).json({ status: 'You are not authorized to access this resource.' });
+    if (!req.session.userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
     }
+    next();
 };
 
 module.exports = authenticate;
