8d127d33ced3a12fd2ce85bf0aab20eb5bd2fa8d
Lauri <labe2301@student.miun.se>
2026-02-14 22:21:19 +0100

    Refactored from ES module to CommonJS

--- utils/password-hasher.js
+++ utils/password-hasher.js
@@ -6,7 +6,7 @@ const argon2 = require('argon2');
  * @param {string} plainTextPassword - The plain text password to hash.
  * @returns {Promise<string>} - The hashed password.
  */
-export const hashPassword = async (plainPassword) => {
+const hashPassword = async (plainPassword) => {
   return argon2.hash(plainPassword, {
     type: argon2.argon2id,
     memoryCost: 2 ** 16,
@@ -22,6 +22,8 @@ export const hashPassword = async (plainPassword) => {
  * @param {string} hashedPassword - The hashed password from the DB.
  * @returns {Promise<boolean>} - Whether the password matches.
  */
-export const verifyPassword = async (hashedPassword, plainPassword) => {
+const verifyPassword = async (hashedPassword, plainPassword) => {
   return argon2.verify(hashedPassword, plainPassword);
 };
+
+module.exports = { hashPassword, verifyPassword };
