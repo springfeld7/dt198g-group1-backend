cba1ee4c2ffc8de349c875e59431f6b8aebbd7c1
Lauri <labe2301@student.miun.se>
2026-02-16 01:01:15 +0100

    JSDoc

--- routes/interests/interests.js
+++ routes/interests/interests.js
@@ -2,6 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Interest = require("../../models/interest");
 
+/**
+ * @route GET /interests
+ * @desc Fetches all interests from the database.
+ * This route calls the `getAllInterests` method from the `Interest` model to retrieve the list of interests.
+ * If successful, it returns the list of interests. If an error occurs, it returns a 500 status with an error message.
+ * 
+ * @returns {Object} 200 - A JSON array containing all the interests from the database.
+ * @returns {Object} 500 - An error message in case of a failure to retrieve the interests.
+ */
 router.get("/", async (req, res) => {
     try {
         await Interest.getAllInterests(req, res);
