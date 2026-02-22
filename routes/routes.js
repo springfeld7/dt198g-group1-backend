e83db7af6f5b2799975a2a28ae5c3f03027f0be2
Lauri <labe2301@student.miun.se>
2026-02-22 22:26:47 +0100

    [bugfix] Add event-route to routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const authRoutes = require("./auth/auth");
 const userRoutes = require("./users/users");
+const eventRoutes = require("./events/events");
 const interestRoutes = require("./interests/interests");
 
 router.use("/auth", authRoutes);
